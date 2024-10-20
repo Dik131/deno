4c895c96029144533cca4e4c31f45c5a07ac86cd
Dik131 <d.i.k.93@mail.ru>
2024-10-20 20:23:28 +0300

    made binary

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-for await (const entry of Deno.readDir('.')) {
-  console.log(entry)
-}
+let tree = async (dir: string): Promise<string[]> => {
+  const out = [];
+  for await (const entry of Deno.readDir(dir)) {
+    out.push(entry.name);
+  }
+  return out;
+};
+
+const files = await tree(Deno.args[0]);
+console.log(files);
