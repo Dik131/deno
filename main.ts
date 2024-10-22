bebfefc8f1f25b045515dc50584f5f746eab2943
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-10-22 10:20:50 +0300

    replased Deno.readDir with walk function from JSR fs

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 //import chalk from "npm:chalk"
+import { walk } from "@std/fs/walk"
 
 const log = console.log
 let tree = async (dir: string): Promise<string[]> => {
   const out = []
-  for await (const entry of Deno.readDir(dir)) {
+  for await (const entry of walk(dir)) {
     out.push(entry.name)
   }
   return out
@@ -11,5 +12,5 @@ let tree = async (dir: string): Promise<string[]> => {
 
 const files = await tree(Deno.args[0])
 log(files)
-log("%c" + files.join("\n"), "color: red")
+log("%c" + files.join("\n"), "color: red") //but only one CSS property
 //console.log(chalk.bgBlue(files.join("\n")))
